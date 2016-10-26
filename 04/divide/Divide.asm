9dd7618f1aed098101803f12a505e0fd446c8a54
Omer <Omertorrents@gmail.com>
2016-10-26 21:50:59 +0300

    divide algorithm is up!

---
+++ 04/divide/Divide.asm
@@ -0,0 +1,82 @@
+// dividing 2 number using logarithmic algorithm
+// https://www.youtube.com/watch?time_continue=389&v=S5tD47NZx7w on 06:30
+// using Divident and Divisor.
+
+
+// update some vars (after and before.)
+@R1
+D = M
+@divident
+M = D
+@R2
+D = M
+@divisor
+M = D
+
+// starting the algo.
+// init algo vars
+@0
+D = A
+@quotient
+M = D
+@divisor
+D = M
+@current_divisor
+M = D
+@1
+D = A
+@current_quotient_base
+M = D
+@DIVIDE
+0;JMP
+(DIVIDE)
+	@divident
+	D = M
+	@divisor
+	D = D - M
+	@SUCCESS
+	D;JLT // if divident < divisor -> stop the loop!
+	@current_divisor
+	D = M
+	@divident
+	D = D - M
+	@DIVIDE_ELSE // if divident < current_divisor go to else
+	D;JGT
+		(DIVIDE_IF)
+		@current_divisor
+		D = M
+		@divident
+		M = M - D
+		@current_quotient_base
+		D = M
+		@quotient
+		M = M + D
+		@current_divisor
+		M<<;
+		@current_quotient_base
+		M<<;
+		@DIVIDE
+		0;JMP
+	(DIVIDE_ELSE)
+		@current_divisor
+		M>>
+		@current_quotient_base
+		M>>
+		@DIVIDE
+		0;JMP
+(SUCCESS)
+	@quotient
+	D = M
+	@score
+	M = D
+	@END
+	0;JMP
+(END)
+	@score
+	D = M
+	@R3
+	M = D
+	@END
+	0;JMP
+
+
