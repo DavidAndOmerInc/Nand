14cfc9895e9c2ba53e5aff9bd3300d629bdf710d
david <david.wies1@gmail.com>
2016-10-26 09:49:22 +0300

    mult for positive numbers

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@0
+D = A
+@i
+M = D
+@R2
+M = D
+
+(LOOP)
+	@R1
+	D = M
+	@i
+	D = D - M
+	@INFINITE_LOOP
+	D;JLE
+	@R0
+	D = M
+	@R2
+	M = M + D
+	@1
+	D = A
+	@i
+	M = M + D
+	@LOOP
+	0;JMP
+
+(INFINITE_LOOP)
+	@INFINITE_LOOP
+	0;JMP
