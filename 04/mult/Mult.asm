f4e88c08294ef2ce6d22af107d350dcf863582be
David <david@localhost.localdomain>
2016-11-19 23:05:26 +0200

    Add Mult

---
+++ 04/mult/Mult.asm
@@ -0,0 +1,61 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// pre loop
+@R0
+D = A
+@i
+M = D
+@R2
+M = D
+
+@sign
+M = 0
+
+@R1
+D = M
+@LOOP
+D;JGE
+	@R1
+	D = M
+	M = -D
+	@sign
+	M = -1
+	@LOOP
+	0;JMP
+
+(LOOP)
+	@R1
+	D = M
+	@i
+	D = D - M
+	@SIGN_CHECK
+	D;JLE
+	@R0
+	D = M
+	@R2
+	M = M + D
+	@1
+	D = A
+	@i
+	M = M + D
+	@LOOP
+	0;JMP
+	
+	
+(SIGN_CHECK)
+	@sign
+	D = M
+	@INFINITE_LOOP
+	D;JEQ
+	@R2
+	M = -M
+
+(INFINITE_LOOP)
+	@INFINITE_LOOP
+	0;JMP
