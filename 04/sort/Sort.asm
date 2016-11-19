042dddadeb3d2ad4a9004a8548f5d7a75a3e8427
David <david@localhost.localdomain>
2016-11-19 22:56:18 +0200

    Fix Sort.asm

--- 04/sort/Sort.asm
+++ 04/sort/Sort.asm
@@ -1,4 +1,4 @@
-// Sort array that start at address that in R14, and his length is in R15.
+// Sort in descending order array that start at address that in R14, and his length is in R15.
 
 @tmp
 M=0
@@ -8,6 +8,8 @@ D = M
 M = D
 @toCompare
 M = 0
+@R15
+D = M
 @endArray
 M = D
 @cur
@@ -15,6 +17,13 @@ D = M
 @endArray
 M = M + D
 
+//@R15
+//D = M
+//@END
+//D;JEQ // Jump if the array is empty.
+//@R1
+//D = D - 1
+//D;JEQ // Jump if the array contain only 1 value.
 
 (OUTSIDE_LOOP)
 	@cur
@@ -22,29 +31,26 @@ M = M + D
 	@toCompare
 	M = D + 1
 	@endArray
-	D = D - M
-	//@R15
-	//D = D - M
+	D = D - M // D now equal to the last place of the array minus the current place.
 	@END
-	D;JEQ
+	D;JEQ // Jump if arrive to last place of the array.
 	(INSIDE_LOOP)
 		@toCompare
 		D = M
 		@endArray
+		D = M - D // D now equal to the last place of the array minus the current index of the array to compare.
+		@TO_OUTSIDE_LOOP
+		D;JEQ // Jump if arrive to last place of the array.
+		@toCompare
+		A = M // go to the place of the to compare.
+		D = M
+		@cur
+		A = M
 		D = D - M
-		@TO_OUT_LOOP
-		D;JEQ
-
+		@CONTINEU_INSIDE_LOOP
+		D;JLE // Jump if the current value bigger than the toCompare value.
+		// Do the swap.
 		@cur
-		A = M
-		D = M
-		@toCompare
-		A = M
-		D = M - D
-		@CONTINEU
-		D;JLE
-
-		@cur // do the swap
 		A = M
 		D = M
 		@tmp
@@ -54,24 +60,21 @@ M = M + D
 		D = M
 		@cur
 		A = M
-		M = D
+		M = D // the cur get the value of toCompare.
 		@tmp
 		D = M
 		@toCompare
 		A = M
 		M = D
-
-		(CONTINEU)
+		(CONTINEU_INSIDE_LOOP)
 		@toCompare
 		M = M + 1
 		@INSIDE_LOOP
 		0;JMP
-		(TO_OUT_LOOP)
-			@cur
-			M = M + 1
-			@OUTSIDE_LOOP
-			0;JMP
+	(TO_OUTSIDE_LOOP)
+	@cur
+	M = M + 1
+	@OUTSIDE_LOOP
+	0;JMP
 
-
-(END)
-
+(END) // The end of the program.
---
+++ Submit.py
@@ -0,0 +1,123 @@
+# To use this script use python3 run this script from "nand2tetris/projects/".
+# Fill the next variable with your and your partner details.
+
+PAIR = True  # Change to 'False' if you work alone.
+
+# First
+FIRST_USER_NAME = "david"
+FIRST_FULL_NAME = "David Wies"
+FIRST_ID = "311511802"
+FIRST_EMAIL = "david.weis@mail.huji.ac.il"
+
+# Second
+SECOND_USER_NAME = "omer"
+SECOND_FULL_NAME = "Omer Shacham"
+SECOND_ID = "20403472"
+SECOND_EMAIL = "omer.shacham@mail.huji.ac.il"
+
+ends = {'.asm', '.hdl'}  # Write the ends of the type of files that you want to submit here.
+
+dont_submit = {}  # Write the names of files you dont want to submit here.
+
+# Note: This isn't an offical program and may contain bugs or create the files in different format
+# than the course staff want. The use of this program is on your own responseility.
+
+# Create by David Wies
+
+
+# -------------------------------------------- Don't touch the code ---------------------------------------------
+
+import os
+import sys
+import tarfile
+
+
+def to_add(filename):
+    for end_file in ends:
+        if filename.endswith(end_file) and not dont_add(filename):
+            return True
+    return False
+
+
+def dont_add(filename):
+    for name in dont_submit:
+        if filename == name:
+            return True
+    return False
+
+
+def run_over_folder(address, to_do_readme, indentation=0):
+    folder_files = ''
+    to_submit = ''
+    files = os.listdir(address)
+    for file_name in files:
+        if os.path.isdir(address + '/' + file_name):
+            inner_files, inner_submit = run_over_folder(address + '/' + file_name, to_do_readme, indentation + 1)
+            if len(inner_submit) > 0:
+                folder_files += '\t' * indentation + file_name + ':\n'
+                folder_files += inner_files
+                to_submit += inner_submit
+        elif to_add(file_name):
+            if to_do_readme:
+                description = input('Enter description for ' + file_name + ' (enter "--skip" to skip the file): ')
+                if description == '--skip':
+                    continue
+                folder_files += '\t' * indentation + file_name + ' - ' + description + '\n'
+            to_submit += address + '/' + file_name + ' '
+    return folder_files, to_submit
+
+
+if __name__ == "__main__":
+    ex_number = input('Which project do you want to submit? ')
+    while not ex_number.isdigit():
+        if ex_number == 'exit':
+            exit(0)
+        ex_number = input("The given input wasn't number, please enter the number of"
+                          ' the project that you want to submit? ')
+    if len(ex_number) == 1:
+        ex = '0' + str(ex_number)
+    else:
+        ex = str(ex_number)
+    do_readme = not (os.path.isfile(ex + '/README'))
+    readme = None
+    if not do_readme:
+        overwrite_readme = input('Do you want to overwrite the readme? (Enter "yes" or "no") ')
+        if overwrite_readme == 'yes':
+            do_readme = True
+    if do_readme:
+        try:
+            readme = open(ex + '/README', 'w')
+        except FileNotFoundError:
+            print('There is no project with that name.', file=sys.stderr)
+            exit(-1)
+        if PAIR:
+            readme.write(FIRST_USER_NAME + ', ' + SECOND_USER_NAME + '\n')
+            readme.write('===============================================================================' + '\n')
+            readme.write(FIRST_FULL_NAME + ', ID ' + FIRST_ID + ', ' + FIRST_EMAIL + '\n')
+            readme.write(SECOND_FULL_NAME + ', ID ' + SECOND_ID + ', ' + SECOND_EMAIL + '\n')
+        else:
+            readme.write(FIRST_USER_NAME + '\n')
+            readme.write('===============================================================================' + '\n')
+            readme.write(FIRST_FULL_NAME + ', ID ' + FIRST_ID + ', ' + FIRST_EMAIL + '\n')
+        readme.write('===============================================================================' + '\n' + '\n')
+        readme.write('                           Project ' + ex_number + ' - An HDL File' + '\n')
+        readme.write('                           -----------------------\n\n\n')
+        readme.write('Submitted Files\n---------------\n')
+        readme.write('README - This file.\n')
+    files_to_readme, files_to_submit = run_over_folder(ex, do_readme)
+    if do_readme:
+        readme.write(files_to_readme)
+        remark = input('Enter the remarks separated by hash tag: ')
+        if len(remark) > 0:
+            readme.write('\n\nRemarks\n-------\n')
+            remarks = remark.split('#')
+            for remark in remarks:
+                readme.write('* ' + remark + '\n')
+        readme.close()
+    os.chdir(ex)
+    tar = tarfile.open('project' + ex_number + '.tar', 'w')
+    files_to_submit = files_to_submit.split(' ')[:-1]
+    for file in files_to_submit:
+        tar.add(file[len(ex) + 1:])
+    tar.add('README')
+    tar.close()
